ac8c617d8a6a07125418e216481f60742fecd56e
Manjeet Singh <mansingh53@publicisgroupe.net>
2020-10-30 09:57:33 +0530

    [MANJEET] : First commit

---
+++ src/main/java/com/sysmind/application/SysMindAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.sysmind.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysMindAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysMindAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sysmind/restcontroller/BaseController.java
@@ -0,0 +1,5 @@
+package com.sysmind.restcontroller;
+
+public class BaseController {
+
+}
